8a8a95eb5816c5045e7f920543882739914dc86a
mGuibTech <mguibtech@gmail.com>
2025-02-03 17:39:02 -0400

    [feat] Refactor App component to integrate Routes for improved screen navigation

--- App.tsx
+++ App.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import {theme} from './src/theme/theme';
 import {ThemeProvider} from '@shopify/restyle';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ForgotPasswordScreen} from './src/screens/auth/ForgotPasswordScreen/ForgotPasswordScreen';
-import {NewPasswordScreen} from './src/screens/auth/NewPasswordScreen/NewPassowordScreen';
-import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
-import {SecurityFingerprintScreen} from './src/screens/auth/SecurityFingerprintScreen/SecurityFingerprintScreen';
-import {SuccessScreen} from './src/screens/auth/SuccessScreen/SuccessScreen';
+import {Routes} from './src/routes/Routes';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <SafeAreaProvider>
-        <SuccessScreen />
-      </SafeAreaProvider>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
